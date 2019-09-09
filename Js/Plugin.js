aeda97ddd3a2ddd0c9869336c6f8f9b2b64f1fb6
Ahmedtahataha <34556811+Ahmedtahataha@users.noreply.github.com>
2019-09-09 21:29:33 +0200

    Add files via upload

---
+++ Js/Plugin.js
@@ -0,0 +1,17 @@
+/* global $, jquery, console, alert */
+
+$(document).ready(function () {
+    
+    "use strict";
+    
+     // Trigger Nice Scroll Plugin
+    
+    $('html').niceScroll({
+        
+        cursorcolor: '#d3d4d9',
+        cursorwidth: 5,
+        cursorborderradius: 3,
+        cursorborder: '1px solid #d3d4d9'
+    });
+    
+});
